75bf3ea94a86502537d5718105e777c05fc2fd04
William Sebastião Teixeira <williamsebastiaoteixeira@gmail.com>
2023-03-25 23:17:56 -0300

    minor security packages

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [
+    AuthModule, 
+    UsersModule,
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 10,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
